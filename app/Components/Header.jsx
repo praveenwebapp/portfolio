1d167e7bfab5718a0a6ea9a84faf8973e6a59b11
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2025-08-06 05:29:23 +0200

    mailchangess

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import { motion } from "motion/react";
 import bg1 from "../../assets/bg1.png";
 // import resume from '../../public/Praveen Kumar Maddela _Resume.pdf'
-import download from '../../assets/downloads.png'
-import contact_arrow from '../../assets/mail.png'
-import hello from '../../assets/hello.png'
+import download from "../../assets/downloads.png";
+import contact_arrow from "../../assets/mail.png";
+import hello from "../../assets/hello.png";
 
 const Header = () => {
   return (
-    <div className="w-11/12 max-w-3xl text-center mx-auto  flex flex-col items-center justify-center gap-4"  style={{ height: '110vh' }} >
+    <div
+      className="w-11/12 max-w-3xl text-center mx-auto  flex flex-col items-center justify-center gap-4"
+      style={{ height: "110vh" }}
+    >
       {/* <motion.div
         initial={{ scale: 0 }}
         whileInView={{ scale: 1 }}
@@ -26,28 +29,40 @@ const Header = () => {
         Hi! I am Praveen Kumar Maddela{" "}
         <Image src={hello} alt="-" className=" w-6" />
       </motion.h3>
-      <motion.h1   initial={{ y: -30, opacity: 0 }}
+      <motion.h1
+        initial={{ y: -30, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.8, delay: 0.5 }} 
-        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+        transition={{ duration: 0.8, delay: 0.5 }}
+        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
+      >
         {" "}
-        Full Stack | Front End Developer based in Germany
+        Full Stack | web & Mobile | Senior Front End Developer
       </motion.h1>
-      <motion.p 
-       initial={{  opacity: 0 }}
-       whileInView={{ opacity: 1 }}
-       transition={{ duration: 0.6, delay: 0.7 }} className=" mx-w-2xl mx-auto font-Ovo">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        className=" mx-w-2xl mx-auto font-Ovo"
+      >
         {" "}
-        I am a Full-Stack Developer with Around 7 years of experience in front-end and back-end development, testing and
-maintenance support, specializing in technologies like React.js, Next.js, AngularJS, Vue.js, Node.js, and Amazon
-Web Services (AWS), JavaScript, Git, Jira, TypeScript among others
+        I’m a Full-Stack Developer with around 7 years of experience across
+        front-end, back-end, and mobile development. I specialize in building
+        clean, responsive interfaces using React.js, AngularJS, and React
+        Native, and have worked on scalable backend services using Node.js,
+        Express.js, and MongoDB. I’m skilled in TypeScript, JavaScript, and
+        write reliable tests with Jest and Cypress. I follow Scrum practices,
+        understand Azure fundamentals, and handle deployments through Vercel,
+        Netlify, and GitHub Pages. Whether it’s crafting smooth user
+        experiences, integrating APIs, or deploying full-stack apps, I enjoy
+        turning ideas into well-built, maintainable products and love
+        collaborating with teams to build something meaningful.
       </motion.p>
 
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4 ">
         <motion.a
-        initial={{ y: 30, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6, delay: 1 }} 
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
           href="#Contact"
           className="px-10 py-3 border  border-white rounded-full  bg-orange-500 text-white  flex items-center gap-2 dark:bg-white dark:text-black
           "
@@ -55,9 +70,9 @@ Web Services (AWS), JavaScript, Git, Jira, TypeScript among others
           Contact Me <Image src={contact_arrow} alt="" className="w-4" />
         </motion.a>
         <motion.a
-        initial={{ y: 30, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6, delay: 1 }} 
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
           href="/Praveen Kumar Maddela _Resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
