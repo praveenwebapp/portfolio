312d884c12de9ad658f3245049cf104038d289de
praveenwebapp <praveenwebapp@gmail.com>
2025-10-05 00:00:13 +0530

    Update Header.jsx

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1 }}
-          href="/Praveen Kumar Maddela _Resume.pdf"
+          href="/Praveen_Kumar_Maddela _Resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
